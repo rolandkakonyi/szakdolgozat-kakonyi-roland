6cd0d20876f35c8badc0a8308532638d5db63b5a
unknown <Roller@.(none)>
2011-12-11 23:08:45 +0100

    original heatmapjs, index alap, controller elso

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <?php ?>
 
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-        <title>Kákonyi Roland Szakdolgozat</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <title>Kákonyi Roland Szakdolgozat</title>
+        <meta name="description" content="Ez az alkalmazás a szakdolgozatom keretében készült.">
+        <META NAME="Author" CONTENT="Kákonyi Roland, rolandkakonyi@gmail.com">
+        
+        
         <link rel="stylesheet" href="/css/reset.css" type="text/css" />
         <link rel="stylesheet" href="/css/style.css" type="text/css" />
 
         <script type="text/javascript" src="/js/jquery-1.7.js"></script>
         <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-
+        <script type="text/javascript" src="/js/heatmap.js"></script>
+        <script type="text/javascript" src="/js/heatmap-gmaps.js"></script>
+        <script type="text/javascript" src="/js/HeatmapController.js"></script>
     </head> 
     <body>
         <div id="main">
             <h1>GMaps Heatmap Overlay</h1>
-            <a href="http://www.patrick-wied.at/static/heatmapjs/" title="heatmap.js">Back to the project page</a><br /><br />
+            
             <div id="heatmapArea">
 
             </div>
             <div id="configArea">
-                <h2>Sidenotes</h2>
-                This is a demonstration of a canvas heatmap gmaps overlay<br /><br />
-                <strong>Note: this is an early release of the heatmap layer. Please feel free to <a href="https://github.com/pa7/heatmap.js" target="_blank">contribute patches</a>. (e.g: correct datapoint pixels after dragrelease (in draw))</strong>
                 <div id="tog" class="btn">Toggle HeatmapOverlay</div>
                 <div id="gen" class="btn">Add 5 random lat/lng coordinates</div>
             </div>
         </div>
-        <script type="text/javascript" src="/js/heatmap.js"></script>
-        <script type="text/javascript" src="/js/heatmap-gmaps.js"></script>
-        <script type="text/javascript" src="/js/HeatmapController.js"></script>
+        
         <script type="text/javascript">
             $(document).ready(function(){
                 HeatmapController.init();
             });
-            
-            
-            
         </script>
     </body>
 </html>
